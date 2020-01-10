3d5023084249b32970eb00f55e5babc2fe37870f
nongnong2 <49285119+nongnong2@users.noreply.github.com>
2020-01-10 16:56:10 +0900

    Evt ID check, sig check, PE check

--- test.py
+++ test.py
@@ -3,8 +3,9 @@ import Evtx.Views as e_views
 from bs4 import BeautifulSoup
 import subprocess
 import re
+import os
 
-def check_EvidnSig():
+def check_Evid():
     path = r"C:\Windows\System32\winevt\Logs\Security.evtx"
     R_EventID = re.compile("[0-9]+(?=<)")
     R_NewProcessName = re.compile("(?<=<Data Name=>).+(?=<)")
@@ -20,16 +21,32 @@ def check_EvidnSig():
                 CreationTime = R_CreationTime.findall(xmlStr)
                 print(CreationTime)
                 print(NewProcessName)
-                #sigcheck 사용 
-                cmd = ['sigcheck.exe', NewProcessName]
-                fd_popen = subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout 
-                data = fd_popen.read().strip() 
-                fd_popen.close() 
-                print(data)
+
+def Check_PE():
+    root = "./"
+    PE_files = []
+    for (root, dir, Objects) in os.walk(root):
+        for Object in Objects:
+            path = os.path.join(root, Object)            
+            check_file = open(path, 'rb')
+            byte = check_file.read(2)
+
+            if byte == b'MZ':
+                PE_files.append(Object)
+
+            else: 
+                continue
+
+    for PE_file in PE_files:
+        path = os.path.join(root, Object)            
+        cmd = ['sigcheck.exe', path]
+        fd_popen = subprocess.Popen(cmd, stdout=subprocess.PIPE).stdout 
+        data = fd_popen.read().strip() 
+        fd_popen.close() 
+        print(PE_file, ':', data)
 
 def main():
-    while True:
-        check_EvidnSig()
+    Check_PE()
 
 if __name__ == "__main__":
     main()
