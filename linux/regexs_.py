1ed216f7a06fa05a357fc9d732c895429772e877
nongnong2 <49285119+nongnong2@users.noreply.github.com>
2020-02-08 02:54:56 +0900

    linux
    linux 1차 업로드

---
+++ linux/regexs_.py
@@ -0,0 +1,11 @@
+ip_regex = r"\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}"
+Time_regex = r"\d{1,2}\/\D+\d{4}\:\d{1,2}:\d{1,2}[:]\d{1,2} [+|-]\d+"
+Method_regex = r"(GET|POST)+"
+Path_regex = r"\/\D+\/\D+[.]\D+"
+Status_regex = r"\d+"
+DataSize_regex = r"\d+"
+Suspicious_Extension_regex = r"(.php|.phtml|.inc|.jsp|.jsf|.asp|.asa|.cds|.cer|.aspx|.asax|.ascx|.ashx|.asmx|.axd|.config|.cs|.csproj|.licx|.licx|.rem|.resources|.resx|.soap|.vb|.vbproj|.vsdisco|.webinfo)"
+
+
+
+
