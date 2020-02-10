5f4ba3ab3eee58d9cd0e91b0fb816fca4660017b
nongnong2 <49285119+nongnong2@users.noreply.github.com>
2020-02-10 01:02:33 -0800

    Add files via upload

--- linux/GetVarLog.py
+++ linux/GetVarLog.py
@@ -1,6 +1,22 @@
 import subprocess
 import time
-import GetService from GetService
+
+def GetServiceList():
+    Service_list = []
+    result = str(subprocess.check_output('service --status-all|grep +', shell='True'),encoding='utf-8')
+    if "apache2" in result :
+        Service_list.append("apache2")
+
+    if "mongodb" in result :
+        Service_list.append("mongodb")
+
+    if "mysql" in result :
+        Service_list.append("mysql")
+
+    if "mariadb" in result :
+        Service_list.append("mariadb")
+
+    return Service_list
 
 def CheckSetUID(count):
     subprocess.call('find / -perm -4000 > /home/investigate/setUid{}.txt'.format(count), shell=True)
@@ -13,32 +29,32 @@ def CollectHttpdLog(logname, count):
     
 if __name__ == "__main__":
     time_input = int(input("time cycle? : "))
-    for i in range(1, 4):
-        CollectVarLog("wtmp", i)
-        CollectVarLog("btmp", i)
-        CollectVarLog("lastlog", i)
-        CollectVarLog("xferlog", i)
-        CollectVarLog("cron", i)
-        CollectVarLog("secure", i)
-        CollectVarLog("messages", i)
-        CollectHttpdLog("access_log", i)
-        CollectHttpdLog("error_log", i)
-        CheckSetUID(i)
-        time.sleep(time_input)
+    # for i in range(1, 4):
+    #     CollectVarLog("wtmp", i)
+    #     CollectVarLog("btmp", i)
+    #     CollectVarLog("lastlog", i)
+    #     CollectVarLog("xferlog", i)
+    #     CollectVarLog("cron", i)
+    #     CollectVarLog("secure", i)
+    #     CollectVarLog("messages", i)
+    #     CollectHttpdLog("access_log", i)
+    #     CollectHttpdLog("error_log", i)
+    #     CheckSetUID(i)
+    #     time.sleep(time_input)
     
-    Service_list = GetService()
+    Service_list = GetServiceList()
     if "mongodb" in Service_list:
-        subprocess.call("cp /var/log/mongodb/error.log /home/investigate/")
+        subprocess.call("cp /var/log/mongodb/error.log /home/investigate/", shell=True)
 
     elif "mysql" in Service_list:
-        subprocess.call("cp /var/log/mysql/error.log /home/investigate")
+        subprocess.call("cp /var/log/mysql/error.log /home/investigate/", shell=True)
 
-    elif "mariadb" om Service_list:
-        subprocess.call("cp /var/log/mariadb/error.log /home/investigate")
+    elif "mariadb" in Service_list:
+        subprocess.call("cp /var/log/mariadb/error.log /home/investigate/", shell=True)
 
-    subprocess.call("netstat -nalp > /home/investigate/final_netsta.txt")
-    subprocess.call("ps -aux > /home/investigate/final_process.txt")
-    subprocess.call("cp /var/spool/cron /home/investigate/")
+    subprocess.call("netstat -nalp > /home/investigate/final_netsta.txt", shell=True)
+    subprocess.call("ps -aux > /home/investigate/final_process.txt", shell=True)
+    subprocess.call("cp /var/spool/cron /home/investigate/", shell=True)
 
     """
     mongodb : /var/log/mongodb/mongod.log
