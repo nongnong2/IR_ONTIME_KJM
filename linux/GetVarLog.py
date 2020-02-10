2f02faa61385a3c7ca1b6155f5e33f5ccdaf77e5
nongnong2 <49285119+nongnong2@users.noreply.github.com>
2020-02-10 00:56:40 -0800

    Add files via upload

--- linux/GetVarLog.py
+++ linux/GetVarLog.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+import GetService from GetService
 
 def CheckSetUID(count):
     subprocess.call('find / -perm -4000 > /home/investigate/setUid{}.txt'.format(count), shell=True)
@@ -25,10 +26,26 @@ if __name__ == "__main__":
         CheckSetUID(i)
         time.sleep(time_input)
     
+    Service_list = GetService()
+    if "mongodb" in Service_list:
+        subprocess.call("cp /var/log/mongodb/error.log /home/investigate/")
+
+    elif "mysql" in Service_list:
+        subprocess.call("cp /var/log/mysql/error.log /home/investigate")
+
+    elif "mariadb" om Service_list:
+        subprocess.call("cp /var/log/mariadb/error.log /home/investigate")
+
     subprocess.call("netstat -nalp > /home/investigate/final_netsta.txt")
     subprocess.call("ps -aux > /home/investigate/final_process.txt")
     subprocess.call("cp /var/spool/cron /home/investigate/")
 
+    """
+    mongodb : /var/log/mongodb/mongod.log
+    mysql : /var/log/mongodb/mysql
+    mariadb : /var/log/mongodb/mysql
+    """
+#Find conf file and do apply the path to code....
 
         
 
